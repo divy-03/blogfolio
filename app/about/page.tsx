b8e5e583a60a6b6fa5cc6160c556a70fa951b9df
divy-03 <pathakdivy03@gmail.com>
2026-01-15 01:52:26 +0530

    patched react2shell-next

---
+++ app/about/page.tsx
@@ -0,0 +1,5 @@
+export default function About() {
+  <img
+    src="https://raw.githubusercontent.com/divy-03/divy-03/main/dark_mode.svg?ts=123"
+  />
+}
--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,9 @@ export default function Home() {
     // <div className="home w-screen h-screen flex items-center justify-center relative">
     <div>
       <Hero />
+      <img
+        src="https://raw.githubusercontent.com/divy-03/divy-03/main/dark_mode.svg?ts=123"
+      />
       <section className="w-screen min-h-screen flex flex-col items-center justify-center relative">
         <div className="flex flex-col items-center">
           <h2 className="text-3xl pt-8 font-bold font-(family-name:--monocraft)">My Projects</h2>
